421ef72d26321003dc78332fb67f15d011f1dd7e
Bizzy-coding <bizzycoding@gmail.com>
2023-10-26 19:52:40 +0100

    using api and api productivity

--- protocol-js/src/components/Navigation.jsx
+++ protocol-js/src/components/Navigation.jsx
@@ -210,6 +210,7 @@ export const navigation = [
     links: [
       { title: 'API Productivity', href: '/API-productivity' },
       { title: 'Authentication', href: '/authentication' },
+      { title: 'Using the API', href: '/using-the-api' },
       { title: 'Lix Account API', href: '/lix-account' },
       { title: 'LinkedIn Account API', href: '/linkedin-account' },
       { title: 'Search API', href: '/search-api' },
