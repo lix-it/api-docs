9edb49f6821095ef0908ded3a1ba8a4b99c79c15
Bizzy-coding <bizzycoding@gmail.com>
2023-10-27 09:20:26 +0100

    light/dark mode logo fix

--- protocol-js/src/components/Logo.jsx
+++ protocol-js/src/components/Logo.jsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import logo from '../images/logos/lix-logo-white.png';
+import darkLogo from '../images/logos/lix-logo-white.png';
+import lightLogo from '../images/logos/lix-logo.png';
 
-export function Logo(props) {
+function Logo(props) {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const observer = new MutationObserver(mutationsList => {
+      for(let mutation of mutationsList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+          console.log("darkMode")
+          setDarkMode(document.documentElement.classList.contains('dark'));
+        }
+      }
+    });
+
+    observer.observe(document.documentElement, { attributes: true });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+  
   return (
-    <Image src={logo} alt="" width={140} height={'auto'} />
-  )
+    <Image
+      src={darkMode ? darkLogo : lightLogo}
+      alt="logo"
+      width={50}  // Replace with your logo's width
+      height={50}  // Replace with your logo's height
+      {...props}
+    />
+  );
 }
+
+export { Logo };
